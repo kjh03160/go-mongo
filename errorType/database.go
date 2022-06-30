68263f4dcd98e8d9c1262ec4ea45f5683d71643e
kjh03160 <kis03160@gmail.com>
2022-06-30 23:35:13 +0900

    [#5] add db error and check logic

--- errorType/database.go
+++ errorType/database.go
@@ -2,6 +2,7 @@ package errorType
 
 import (
 	"fmt"
+
 	"github.com/pkg/errors"
 )
 
@@ -10,45 +11,78 @@ var SingleResultErr = errors.New("single result is nil")
 type basicQueryInfo struct {
 	collection string
 	filter     interface{}
+	update     interface{}
+	doc        interface{}
 }
+
 type notFoundError struct {
 	basicQueryInfo
 }
 
-type internalError struct {
+type duplicatedKeyError struct {
 	basicQueryInfo
-	update interface{}
 	error
 }
 
-func NotFoundError(col string, filter interface{}) error {
+type internalError struct {
+	basicQueryInfo
+	error
+}
+
+func NotFoundError(col string, filter, update, doc interface{}) error {
 	err := notFoundError{}
-	err.collection = col
 	err.filter = filter
+	err.collection = col
+	err.update = update
+	err.doc = doc
 	return err
 }
 
-func InternalError(col string, filter interface{}, mongoErr error, args ...interface{}) error {
+func DuplicatedKeyError(col string, filter, update, doc interface{}, mongoErr error) error {
+	err := duplicatedKeyError{}
+	err.filter = filter
+	err.collection = col
+	err.update = update
+	err.doc = doc
+	err.error = mongoErr
+	return err
+}
+
+func InternalError(col string, filter, update, doc interface{}, mongoErr error) error {
 	err := internalError{}
 	err.collection = col
 	err.filter = filter
-	err.update = args
+	err.update = update
+	err.doc = doc
 	err.error = mongoErr
 	return err
 }
 
 func (e notFoundError) Error() string {
-	return fmt.Sprintf("%s not found. filter: %+v", e.collection, e.filter)
+	return fmt.Sprintf("%s not found. ", e.collection) + getBasicInfoErrorMsg(e.basicQueryInfo)
+}
+
+func (e duplicatedKeyError) Error() string {
+	return fmt.Sprintf("%s failed to write due to duplicated key, err: %s ", e.collection, e.error.Error()) + getBasicInfoErrorMsg(e.basicQueryInfo)
 }
 
 func (e internalError) Error() string {
-	msg := fmt.Sprintf("mongo internal err: %s", e.error.Error())
+	return fmt.Sprintf("mongo internal err: %s ", e.error.Error()) + getBasicInfoErrorMsg(e.basicQueryInfo)
+}
 
+func getBasicInfoErrorMsg(e basicQueryInfo) string {
+	msg := "| {query info: "
 	if e.filter != nil {
 		msg += fmt.Sprintf(" filter: %+v", e.filter)
 	}
+
 	if e.update != nil {
-		msg += fmt.Sprintf(" update: %+v", e.update)
+		msg += fmt.Sprintf(", update: %+v", e.update)
 	}
+
+	if e.doc != nil {
+		msg += fmt.Sprintf(", doc: %+v", e.doc)
+	}
+	msg += "}"
 	return msg
 }
--- mongo/operation.go
+++ mongo/operation.go
@@ -2,66 +2,71 @@ package mongo
 
 import (
 	"context"
+
+	"mongo-orm/errorType"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"mongo-orm/errorType"
 )
 
 type Collection struct {
 	*mongo.Collection
 }
 
-//func (col *Collection) evaluateSingleResult(result *mongo.SingleResult) error {
-//	if result == nil {
-//		return errorType.InternalError(col.Name(), filter, errorType.SingleResultErr)
-//	}
-//	return nil
-//}
+func (col *Collection) evaluateAndDecodeSingleResult(result *mongo.SingleResult, v interface{}) error {
+	if result == nil {
+		return errorType.SingleResultErr
+	}
+	if err := result.Decode(v); err != nil {
+		return err
+	}
+	return nil
+}
 
 func (col *Collection) FindAll(filter interface{}, opts ...*options.FindOptions) (*mongo.Cursor, error) {
 	cursor, err := col.Collection.Find(context.Background(), filter, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Name(), filter, err)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, nil)
 	}
 	return cursor, nil
 }
 
-func (col *Collection) FindOne(filter interface{}, opts ...*options.FindOneOptions) (*mongo.SingleResult, error) {
+func (col *Collection) FindOne(data, filter interface{}, opts ...*options.FindOneOptions) error {
 	singleResult := col.Collection.FindOne(context.Background(), filter, opts...)
-	if singleResult == nil {
-		return nil, errorType.InternalError(col.Name(), filter, errorType.SingleResultErr)
+	if err := col.evaluateAndDecodeSingleResult(singleResult, data); err != nil {
+		return errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, nil)
 	}
-	return singleResult, nil
+	return nil
 }
 
-func (col *Collection) FindOneAndModify(filter interface{}, update interface{}, opts ...*options.FindOneAndUpdateOptions) (*mongo.SingleResult, error) {
+func (col *Collection) FindOneAndModify(data, filter interface{}, update interface{}, opts ...*options.FindOneAndUpdateOptions) error {
 	singleResult := col.Collection.FindOneAndUpdate(context.Background(), filter, update, opts...)
-	if singleResult == nil {
-		return nil, errorType.InternalError(col.Name(), filter, errorType.SingleResultErr)
+	if err := col.evaluateAndDecodeSingleResult(singleResult, data); err != nil {
+		return errorType.ParseAndReturnDBError(err, col.Name(), filter, update, nil)
 	}
-	return singleResult, nil
+	return nil
 }
 
-func (col *Collection) FindOneAndReplace(filter interface{}, replacement interface{}, opts ...*options.FindOneAndReplaceOptions) (*mongo.SingleResult, error) {
+func (col *Collection) FindOneAndReplace(data, filter interface{}, replacement interface{}, opts ...*options.FindOneAndReplaceOptions) error {
 	singleResult := col.Collection.FindOneAndReplace(context.Background(), filter, replacement, opts...)
-	if singleResult == nil {
-		return nil, errorType.InternalError(col.Name(), filter, errorType.SingleResultErr)
+	if err := col.evaluateAndDecodeSingleResult(singleResult, data); err != nil {
+		return errorType.ParseAndReturnDBError(err, col.Name(), filter, replacement, nil)
 	}
-	return singleResult, nil
+	return nil
 }
 
-func (col *Collection) FindOneAndDelete(filter interface{}, opts ...*options.FindOneAndDeleteOptions) (*mongo.SingleResult, error) {
+func (col *Collection) FindOneAndDelete(data, filter interface{}, opts ...*options.FindOneAndDeleteOptions) error {
 	singleResult := col.Collection.FindOneAndDelete(context.Background(), filter, opts...)
-	if singleResult == nil {
-		return nil, errorType.InternalError(col.Name(), filter, errorType.SingleResultErr)
+	if err := col.evaluateAndDecodeSingleResult(singleResult, data); err != nil {
+		return errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, nil)
 	}
-	return singleResult, nil
+	return nil
 }
 
 func (col *Collection) UpdateOne(filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
 	updateResult, err := col.Collection.UpdateOne(context.Background(), filter, update, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Collection.Name(), filter, err, update)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, update, nil)
 	}
 	return updateResult, nil
 }
@@ -69,7 +74,7 @@ func (col *Collection) UpdateOne(filter interface{}, update interface{}, opts ..
 func (col *Collection) UpdateMany(filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
 	updateResult, err := col.Collection.UpdateMany(context.Background(), filter, update, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Collection.Name(), filter, err, update)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, update, nil)
 	}
 	return updateResult, nil
 }
@@ -77,7 +82,7 @@ func (col *Collection) UpdateMany(filter interface{}, update interface{}, opts .
 func (col *Collection) ReplaceOne(filter interface{}, document interface{}, opts ...*options.ReplaceOptions) (*mongo.UpdateResult, error) {
 	result, err := col.Collection.ReplaceOne(context.Background(), filter, document, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Collection.Name(), filter, err, document)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, document)
 	}
 	return result, nil
 }
@@ -85,7 +90,7 @@ func (col *Collection) ReplaceOne(filter interface{}, document interface{}, opts
 func (col *Collection) DeleteOne(filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
 	deleteResult, err := col.Collection.DeleteOne(context.Background(), filter, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Collection.Name(), filter, err)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, nil)
 	}
 	return deleteResult, nil
 }
@@ -93,7 +98,7 @@ func (col *Collection) DeleteOne(filter interface{}, opts ...*options.DeleteOpti
 func (col *Collection) DeleteMany(filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
 	deleteResult, err := col.Collection.DeleteMany(context.Background(), filter, opts...)
 	if err != nil {
-		return nil, errorType.InternalError(col.Collection.Name(), filter, err)
+		return nil, errorType.ParseAndReturnDBError(err, col.Name(), filter, nil, nil)
 	}
 	return deleteResult, nil
 }
